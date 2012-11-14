4f1484e97e564c110fce61668ca4d73b38288021
anonfunc <anonfunc@gmail.com>
2012-11-14 00:27:50 -0800

    Rewrite to use org-babel and tangle.

---
+++ home/.emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/org/habits.org" "~/org/calendars/gcal.org" "~/org/calendars/exchange.org" "~/org/personal.org" "~/org/work.org" "~/org/shopping.org" "~/org/inbox.org" "~/org/from-mobile.org")))
+ '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
+ '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
