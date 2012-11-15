e23548a1dd13da595af31806516671cf4b01d9d8
anonfunc <anonfunc@gmail.com>
2012-11-14 18:48:23 -0800

    Color customize tweaks.

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
  '(org-agenda-files (quote ("~/org/habits.org" "~/org/calendars/gcal.org" "~/org/calendars/exchange.org" "~/org/personal.org" "~/org/work.org" "~/org/shopping.org" "~/org/inbox.org" "~/org/from-mobile.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t)))))
@@ -11,4 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(highlight ((t (:background "Black")))))
