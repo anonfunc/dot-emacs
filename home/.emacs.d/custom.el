857265be92fd4a3aa22189debba77251d80a7126
anonfunc <anonfunc@gmail.com>
2012-12-03 13:29:40 -0800

    Smartparens.

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,13 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cua-delete-selection nil)
  '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
  '(org-agenda-files (quote ("~/org/habits.org" "~/org/calendars/gcal.org" "~/org/calendars/exchange.org" "~/org/personal.org" "~/org/work.org" "~/org/shopping.org" "~/org/inbox.org" "~/org/from-mobile.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
- '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t)))))
+ '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t))))
+ '(sp-highlight-pair-overlay nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(highlight ((t (:background "Black")))))
+ '(highlight ((t (:background "Black"))))
+ '(sp-pair-overlay-face ((t nil))))
