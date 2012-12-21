8624d06c722fe06f9da86186ee659a7e6cf17fd7
anonfunc <anonfunc@gmail.com>
2012-12-21 11:11:55 -0800

    Agenda files.

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(cua-delete-selection nil)
  '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
- '(org-agenda-files (quote ("~/org/habits.org" "~/org/calendars/gcal.org" "~/org/calendars/exchange.org" "~/org/personal.org" "~/org/work.org" "~/org/shopping.org" "~/org/inbox.org" "~/org/from-mobile.org")))
+ '(org-agenda-files (quote ("~/org/velocity.org" "~/org/habits.org" "~/org/calendars/gcal.org" "~/org/calendars/exchange.org" "~/org/personal.org" "~/org/work.org" "~/org/shopping.org" "~/org/inbox.org" "~/org/from-mobile.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t))))
  '(sp-highlight-pair-overlay nil))
