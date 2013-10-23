d5fcc6e564c3bc1c899366b9b65459ee95b15669
anonfunc <anonfunc@gmail.com>
2013-10-23 13:54:56 -0700

    Mac tweaks

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -4,13 +4,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cua-delete-selection nil)
+ '(cua-mode t nil (cua-base))
  '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
  '(helm-command-prefix-key "C-c h")
  '(org-agenda-files (quote ("~/org/captures.org" "~/org/notes.org" "~/org/jiras.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t))))
- '(sp-highlight-pair-overlay nil))
+ '(sp-highlight-pair-overlay nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
