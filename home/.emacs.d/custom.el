36a258839a9883cd70c028b2f52b678048e156a1
anonfunc <anonfunc@gmail.com>
2013-12-05 20:54:58 -0800

    Custom changes.

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -8,7 +8,7 @@
  '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
  '(helm-command-prefix-key "C-c h")
- '(org-agenda-files (quote ("~/org/captures.org" "~/org/notes.org" "~/org/jiras.org")))
+ '(org-agenda-files (quote ("~/org/exchange-calendar.org" "~/org/captures.org" "~/org/notes.org" "~/org/jiras.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t))))
  '(sp-highlight-pair-overlay nil)
@@ -19,4 +19,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(highlight ((t (:background "Black"))))
- '(sp-pair-overlay-face ((t nil)) t))
+ '(sp-pair-overlay-face ((t nil))))
