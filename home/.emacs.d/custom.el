7666340eda071d9448e3dfb15c78a25592f89edd
anonfunc <anonfunc@gmail.com>
2014-01-16 15:42:01 -0800

    Color theme back to melpa and powerline.

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -4,17 +4,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(background-mode dark)
+ '(compilation-message-face (quote default))
  '(cua-delete-selection nil)
  '(cua-mode t nil (cua-base))
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(exec-path-from-shell-variables (quote ("PATH")))
  '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
  '(helm-command-prefix-key "C-c h")
+ '(magit-diff-use-overlays nil)
  '(org-agenda-files (quote ("~/org/exchange-calendar.org" "~/org/captures.org" "~/org/notes.org" "~/org/jiras.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t))))
  '(sp-highlight-pair-overlay nil)
  '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-very-old-color nil)
  '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
