c672628942ed230af6c0df8f7a1608b7c0d59481
anonfunc <anonfunc@gmail.com>
2014-01-16 10:26:39 -0800

    Up to date.  Also, setting vc-follow-symlinks doesn't seem to work. Odd.

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,16 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(background-mode dark)
  '(cua-delete-selection nil)
  '(cua-mode t nil (cua-base))
- '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(exec-path-from-shell-variables (quote ("PATH")))
  '(font-lock-maximum-decoration (quote ((dired-mode) (t . t))))
  '(helm-command-prefix-key "C-c h")
  '(org-agenda-files (quote ("~/org/exchange-calendar.org" "~/org/captures.org" "~/org/notes.org" "~/org/jiras.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(safe-local-variable-values (quote ((eval folding-mode t) (folded-file . t))))
  '(sp-highlight-pair-overlay nil)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
